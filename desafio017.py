a4ab5ca4603a8fb2e89ad040fbcbe1434d357606
NathanChincowitz <153536396+NathanChincowitz@users.noreply.github.com>
2024-06-16 16:00:00 -0300

    Update desafio017.py

--- desafio017.py
+++ desafio017.py
@@ -4,7 +4,9 @@ from math import hypot
 a = float(input('digite o valor de A: '))
 b = float(input('digite o valor de B: '))
 
-'''h = (a**2+b**2)**(1/2)'''
+'''
+cod que fiz antes de saber da existencia do hypot
+h = (a**2+b**2)**(1/2)'''
 h = hypot(a, b)
 
 print(f'com tetetos {a} e {b} a hipotenusa dele triangulo retangulo é {h:.2f} ')
